04941b46ed3334c79eef9969d25c0da46018d042
1mamute <gabrielbassinello@hotmail.com>
2024-01-31 03:30:15 +0000

    feat: added group to the message

--- go-grpc-chat-server/internal/chat/chat_service.go
+++ go-grpc-chat-server/internal/chat/chat_service.go
@@ -88,40 +88,61 @@ func (s *Server) getSubscribedPlayers(groupID uint64) []uint64 {
 
 // StreamMessages handles bidirectional streaming for chat messages.
 func (s *Server) StreamMessages(stream ChatService_StreamMessagesServer) error {
-	// Temporary storage for client's stream
-	var clientStream ChatService_StreamMessagesServer
-
 	for {
-		// Receive a message from the stream
 		in, err := stream.Recv()
 		if err != nil {
 			log.Printf("Error receiving from stream: %v", err)
 			return err
 		}
 
-		// Handle different message types
 		switch msg := in.MessageType.(type) {
 		case *MessageStream_Message:
 			// Handle incoming chat message
-			log.Printf("Received message: %v", msg)
-			// [Your message handling logic here]
+			log.Printf("Received message from player %d in group %d", msg.Message.Player.Id, msg.Message.GroupId)
+			s.broadcastMessageToGroup(msg.Message)
 
 		case *MessageStream_StreamRequest:
-			// Handle stream request
-			log.Printf("Stream request from player: %v", msg.StreamRequest.Player.Id)
-			clientStream = stream
-			s.streams.Store(msg.StreamRequest.Player.Id, stream)
+			// Handle stream request, store stream for later use
+			playerID := msg.StreamRequest.Player.Id
+			s.streams.Store(playerID, stream)
 
 		default:
-			// Unknown message type
 			return status.Errorf(codes.InvalidArgument, "Unknown message type")
-		}
-
-		// If clientStream is set, you can send messages back to the client
-		if clientStream != nil {
-			// [Your logic to send messages to the client]
 		}
 	}
 }
 
-// Additional methods for Subscribe, Unsubscribe, etc., can be added here.
+// broadcastMessageToGroup sends a given message to all subscribed players of a group.
+// broadcastMessageToGroup sends a given message to all subscribed players of a group.
+func (s *Server) broadcastMessageToGroup(message *Message, groupID uint64) {
+	// Retrieve subscribed players for the group
+	players, ok := s.groupSubscriptions.Load(groupID)
+	if !ok {
+		log.Printf("No players subscribed to group %d", groupID)
+		return
+	}
+
+	subscribedPlayers, ok := players.([]uint64)
+	if !ok {
+		log.Printf("Invalid type for group subscribers")
+		return
+	}
+
+	for _, playerID := range subscribedPlayers {
+		stream, ok := s.streams.Load(playerID)
+		if !ok {
+			log.Printf("Stream for player %d not found", playerID)
+			continue
+		}
+
+		playerStream, ok := stream.(ChatService_StreamMessagesServer)
+		if !ok {
+			log.Printf("Invalid stream type for player %d", playerID)
+			continue
+		}
+
+		if err := playerStream.Send(&MessageStream{MessageType: &MessageStream_Message{Message: message}}); err != nil {
+			log.Printf("Error sending message to player %d: %v", playerID, err)
+		}
+	}
+}
